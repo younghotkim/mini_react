76c6ef9b70bdfce6f732aba1e33e5c72c044a523
younghotkim <younghotkim@gmail.com>
2024-07-27 10:07:43 +0900

    0727/mini_react/promotion

--- src/routes/LandingPage/Promotion.tsx
+++ src/routes/LandingPage/Promotion.tsx
@@ -135,6 +135,91 @@ export default function Promotion() {
             우린 어디서 왔나 oh, ayy
           </p>
         </Div>
+        <Div
+          width="15%"
+          minWidth="15%"
+          minHeight="20rem"
+          className="section-item flex-col items-center justify-center text-black bg-base-100">
+          <p className="font-extrabold text-center mb-10">이재욱 & 카리나</p>
+
+          <p>
+            꿈에 그리던 이상형을 만났습니다.
+            <br /> 사건은 다가와 ah, oh, ayy 거세게 커져가 ah, oh, ayy <br />
+            질문은 계속돼 ah, oh, ayy <br /> 우린 어디서 왔나 oh, ayy <br />
+            사건은 다가와 ah, oh, ayy <br />
+            거세게 커져가 ah, oh, ayy ,tell me, oh, ayy <br />
+            우린 어디서 왔나 oh, ayy <br />
+            우린 어디서 왔나 oh, ayy
+          </p>
+        </Div>
+        <Div
+          width="15%"
+          minWidth="15%"
+          minHeight="20rem"
+          className="section-item flex-col items-center justify-center text-black bg-base-100">
+          <p className="font-extrabold text-center mb-10">이재욱 & 카리나</p>
+
+          <p>
+            꿈에 그리던 이상형을 만났습니다.
+            <br /> 사건은 다가와 ah, oh, ayy 거세게 커져가 ah, oh, ayy <br />
+            질문은 계속돼 ah, oh, ayy <br /> 우린 어디서 왔나 oh, ayy <br />
+            사건은 다가와 ah, oh, ayy <br />
+            거세게 커져가 ah, oh, ayy ,tell me, oh, ayy <br />
+            우린 어디서 왔나 oh, ayy <br />
+            우린 어디서 왔나 oh, ayy
+          </p>
+        </Div>
+        <Div
+          width="15%"
+          minWidth="15%"
+          minHeight="20rem"
+          className="section-item flex-col items-center justify-center text-black bg-base-100">
+          <p className="font-extrabold text-center mb-10">이재욱 & 카리나</p>
+
+          <p>
+            꿈에 그리던 이상형을 만났습니다.
+            <br /> 사건은 다가와 ah, oh, ayy 거세게 커져가 ah, oh, ayy <br />
+            질문은 계속돼 ah, oh, ayy <br /> 우린 어디서 왔나 oh, ayy <br />
+            사건은 다가와 ah, oh, ayy <br />
+            거세게 커져가 ah, oh, ayy ,tell me, oh, ayy <br />
+            우린 어디서 왔나 oh, ayy <br />
+            우린 어디서 왔나 oh, ayy
+          </p>
+        </Div>
+        <Div
+          width="15%"
+          minWidth="15%"
+          minHeight="20rem"
+          className="section-item flex-col items-center justify-center text-black bg-base-100">
+          <p className="font-extrabold text-center mb-10">이재욱 & 카리나</p>
+
+          <p>
+            꿈에 그리던 이상형을 만났습니다.
+            <br /> 사건은 다가와 ah, oh, ayy 거세게 커져가 ah, oh, ayy <br />
+            질문은 계속돼 ah, oh, ayy <br /> 우린 어디서 왔나 oh, ayy <br />
+            사건은 다가와 ah, oh, ayy <br />
+            거세게 커져가 ah, oh, ayy ,tell me, oh, ayy <br />
+            우린 어디서 왔나 oh, ayy <br />
+            우린 어디서 왔나 oh, ayy
+          </p>
+        </Div>
+        <Div
+          width="15%"
+          minWidth="15%"
+          minHeight="20rem"
+          className="section-item flex-col items-center justify-center text-black bg-base-100">
+          <p className="font-extrabold text-center mb-10">이재욱 & 카리나</p>
+
+          <p>
+            꿈에 그리던 이상형을 만났습니다.
+            <br /> 사건은 다가와 ah, oh, ayy 거세게 커져가 ah, oh, ayy <br />
+            질문은 계속돼 ah, oh, ayy <br /> 우린 어디서 왔나 oh, ayy <br />
+            사건은 다가와 ah, oh, ayy <br />
+            거세게 커져가 ah, oh, ayy ,tell me, oh, ayy <br />
+            우린 어디서 왔나 oh, ayy <br />
+            우린 어디서 왔나 oh, ayy
+          </p>
+        </Div>
       </div>
     </section>
   )
